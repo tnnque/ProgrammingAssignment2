d465ea7d5f796ab63b3fc893d1c2c201cfed9520
tnnque <51043766+tnnque@users.noreply.github.com>
2019-05-27 01:15:12 +0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
 ## functions do create a matrix from input matrix and make it to an inverse matrix of the original
 
-## Write a short comment describing this function
 ## This function defines the input matrix but not in runtime
 # Create an object 'makeCacheMatrix' to store matrix and inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
@@ -10,8 +8,8 @@ makeCacheMatrix <- function(x = matrix()) {
     
     list(stored = stored, inverse = inverse)
 }
-## Write a short comment describing this function
-## This function calculate the inverse matrix if the inverse matrix has not been calculated
+
+## Function below calculate the inverse matrix if the inverse matrix has not been calculated
 cacheSolve <- function(y, ...) {
     if(!is.null(y$inverse)) {
         message("getting cached data")
@@ -22,7 +20,7 @@ cacheSolve <- function(y, ...) {
     }
     y
 }
-        ## Return a matrix that is the inverse of 'x'
+
 
 ## Test
 my_matrix <- matrix(c(3,-4,2,-5), nrow=2, ncol=2)
