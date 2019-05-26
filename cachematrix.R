0f2dcd2745b90c1c98110727f34ddd33786d75a5
tnnque <51043766+tnnque@users.noreply.github.com>
2019-05-27 02:00:34 +0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,39 @@
 ## functions do create a matrix from input matrix and make it to an inverse matrix of the original
 
+### SOLUTION 1
+makeCacheMatrix <- function(x = matrix()) { # This function defines the input matrix but not in runtime
+    i <- NULL
+    set <- function (y) { 
+        x <<- y #Assign the input matrix to store
+        i <<- NULL #Assign null value to i because inverse matrix has not been calculated
+    }
+    get <- function() x
+    setSolve <- function(solve) i <<- solve
+    getSolve <- function() i
+    #Define the function to get and set inverse value of the matrix
+    list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
+}
+## Function below calculate the inverse matrix if the inverse matrix has not been calculated
+cacheSolve <- function(x, ...) {
+    i <- x$getSolve() #Retrive the store value of the input matrix
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+        #Display the message and return value of the inverse matrix when it has already been calculated
+    }
+    data <- x$get()
+    i <- solve(data, ...) #function 'solve'is used to calculate an inverse of the matrix
+    x$setSolve(i)
+    i #Display the value of the inverse matrix that was calculated
+}
+
+## Run test
+my_matrix <- matrix(c(3,-4,2,-5), nrow=2, ncol=2)
+inv <- makeCacheMatrix(x)
+cacheSolve(inv)
+
+#=======================
+### SOLUTION 2 (shorter)
 ## This function defines the input matrix but not in runtime
 # Create an object 'makeCacheMatrix' to store matrix and inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
@@ -20,7 +54,6 @@ cacheSolve <- function(y, ...) {
     }
     y
 }
-
 
 ## Test
 my_matrix <- matrix(c(3,-4,2,-5), nrow=2, ncol=2)
